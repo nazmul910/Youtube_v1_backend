856838972ebc938ba9251892bd7b34f9655abbff
nazmul910 <islamnasir910@gmail.com>
2025-12-07 20:28:06 +0600

    feat: add Cloudinary integration for file uploads and update dependencies

---
+++ src/utils/Cloudinary.js
@@ -0,0 +1,27 @@
+import {v2 as cloudinary} from 'cloudinary';
+import fs from 'fs';
+
+cloudinary.config({
+    cloud_name:process.env.CLOUDINARY_NAME,
+    api_key:process.env.CLOUDINARY_API_KEY,
+    api_secret:process.env.CLOUDINARY_API_SECRET
+});
+
+const uloadOnCloudinary = async (loaclFilePath) =>{
+    try {
+        if(!loaclFilePath) return null;
+
+        const response = await cloudinary.uploader.upload(loaclFilePath,{
+            resource_type:'auto',
+        });
+        console.log('File is upload on Cloudinary ', response.url);
+
+        return response;
+
+    } catch (error) {
+        fs.unlinkSync(loaclFilePath);
+        return null;
+    }
+}
+
+export {uloadOnCloudinary};
