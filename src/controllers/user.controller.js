453691da4297fc15fb3887bb0fb2f2a99f636256
nazmul910 <islamnasir910@gmail.com>
2025-12-11 01:09:24 +0600

    feat: add change password, get current user, update account details, and update user avatar functionalities

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -209,12 +209,93 @@ const refreshAccessToken = asyncHandler(async (req,res) =>{
 
 })
 
+const changeCurrentPassword = asyncHandler( async (req,res) =>{
+    const {oldPassword,newPassowrd,confirmPassword} = req.body;
+
+    // if(!(newPassowrd === confirmPassword)){
+    //     throw new ApiError(401,"Newpassword and confirm password not match!")
+    // }
+
+    const user = await User.findById(req.user?.id);
+    const isPasswordCorrect = await user.isPasswordCorrect(oldPassword);
+
+    if(!isPasswordCorrect){
+        throw new ApiError(400,"Invalid old password");
+    };
+
+    user.password = newPassowrd;
+
+    await user.save({validateBeforeSave:false});
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(
+            200,
+            {},
+            "Password changed successfully"
+        )
+    )
+})
+
+const getCurrentUser = asyncHandler( async (req,res) =>{
+    return res
+    .status(200)
+    .json(200,req.user,"current user fetched successfully")
+})
+
+const updateAccountDetails = asyncHandler( async (req,res) =>{
+    const {fullName,email} = req.body;
+
+    if(!fullName || !email){
+        throw new ApiError(400,"All fields are required");
+    }
+
+    const user = User.findByIdAndUpdate(
+        req.user?._id,
+        {
+            $set: {
+                fullName,
+                email
+            }
+        },
+        {new:true}
+    ).select("-password ")
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(
+            200,
+            user,
+            "Account details updated successfully"
+        )
+    )
+
+})
+
+const updateUserAvatar = asyncHandler(async (req,res) =>{
+    const avatarLocalPath = req.file?.path;
+
+    if(!avatarLocalPath){
+        throw new ApiError(400,"Avater file is missing");
+    }
+
+    const avatar = await uploadOnCloudinary(avatarLocalPath);
+    if(!avatar.url){
+        throw new ApiError(400,"Error while uploading on avator");
+    }
+})
+
 
 
 export {
     registerUser,
     loginUser,
     logoutUser,
-    refreshAccessToken
+    refreshAccessToken,
+    changeCurrentPassword,
+    getCurrentUser,
+    updateAccountDetails 
 
 }; 
